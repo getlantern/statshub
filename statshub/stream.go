b2222a9b2133101173f12a9e15beec1e866bdf42
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-04-09 02:09:54 -0500

    Restored streaming functionality

--- statshub/stream.go
+++ statshub/stream.go
@@ -15,164 +15,164 @@
 
 package statshub
 
-// import (
-// 	"code.google.com/p/go.net/websocket"
-// 	"encoding/json"
-// 	"fmt"
-// 	"github.com/oxtoacart/go-ringbuffer/ringbuff"
-// 	"io"
-// 	"log"
-// 	"net/http"
-// 	"sync"
-// 	"time"
-// )
+import (
+	"code.google.com/p/go.net/websocket"
+	"encoding/json"
+	"fmt"
+	"github.com/oxtoacart/go-ringbuffer/ringbuff"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
 
-// const (
-// 	streamingInterval = 30 * time.Second
-// )
+const (
+	streamingInterval = 30 * time.Second
+)
 
-// var (
-// 	nextStreamingClientId = 0
-// 	streamingClients      = make(map[int]*streamingClient)
-// 	newStreamingClient    = make(chan *streamingClient)
-// 	closedStreamingClient = make(chan int)
-// 	updatesCacheSize      = 1 * 60 * 60 / int(streamingInterval.Seconds()) // 1 hour worth of updates
-// 	oldUpdates            = ringbuff.New(updatesCacheSize)
-// )
+var (
+	nextStreamingClientId = 0
+	streamingClients      = make(map[int]*streamingClient)
+	newStreamingClient    = make(chan *streamingClient)
+	closedStreamingClient = make(chan int)
+	updatesCacheSize      = 1 * 60 * 60 / int(streamingInterval.Seconds()) // 1 hour worth of updates
+	oldUpdates            = ringbuff.New(updatesCacheSize)
+)
 
-// type streamingClient struct {
-// 	ws      *websocket.Conn
-// 	updates chan *streamingUpdate
-// 	id      chan int
-// }
+type streamingClient struct {
+	ws      *websocket.Conn
+	updates chan *streamingUpdate
+	id      chan int
+}
 
-// type streamingUpdate struct {
-// 	asOf    time.Time
-// 	allDims map[string]*json.RawMessage // using pointer because of this bug - https://code.google.com/p/go/issues/detail?id=6528
-// }
+type streamingUpdate struct {
+	asOf    time.Time
+	allDims map[string]*json.RawMessage // using pointer because of this bug - https://code.google.com/p/go/issues/detail?id=6528
+}
 
-// // ClientQueryResponse is a Response to a StatsQuery
-// type StreamingQueryResponse struct {
-// 	Response
-// 	AsOfSeconds int64                       `json:"asOfSeconds"`
-// 	Dims        map[string]*json.RawMessage `json:"dims"`
-// }
+// ClientQueryResponse is a Response to a StatsQuery
+type StreamingQueryResponse struct {
+	Response
+	AsOfSeconds int64                       `json:"asOfSeconds"`
+	Dims        map[string]*json.RawMessage `json:"dims"`
+}
 
-// func init() {
-// 	http.Handle("/stream/", websocket.Handler(streamStats))
-// 	go handleStreamingClients()
-// }
+func init() {
+	http.Handle("/stream/", websocket.Handler(streamStats))
+	go handleStreamingClients()
+}
 
-// // handleStreamingClients handles streaming updates to subscribed streaming clients
-// func handleStreamingClients() {
-// 	for {
-// 		nextInterval := time.Now().Truncate(streamingInterval).Add(streamingInterval)
-// 		waitTime := nextInterval.Sub(time.Now())
-// 		select {
-// 		case client := <-newStreamingClient:
-// 			// Add new client to map
-// 			nextStreamingClientId++
-// 			streamingClients[nextStreamingClientId] = client
-// 			client.id <- nextStreamingClientId
+// handleStreamingClients handles streaming updates to subscribed streaming clients
+func handleStreamingClients() {
+	for {
+		nextInterval := time.Now().Truncate(streamingInterval).Add(streamingInterval)
+		waitTime := nextInterval.Sub(time.Now())
+		select {
+		case client := <-newStreamingClient:
+			// Add new client to map
+			nextStreamingClientId++
+			streamingClients[nextStreamingClientId] = client
+			client.id <- nextStreamingClientId
 
-// 			// Send buffered updates to client
-// 			oldUpdates.ForEach(func(item interface{}) {
-// 				update := item.(*streamingUpdate)
-// 				for _, client := range streamingClients {
-// 					client.updates <- update
-// 				}
-// 			})
-// 		case closedId := <-closedStreamingClient:
-// 			// Remove disconnected client from map
-// 			delete(streamingClients, closedId)
-// 		case <-time.After(waitTime):
-// 			// Query fallback and country dims
-// 			dims, err := QueryDims([]string{"fallback", "country"})
-// 			if err != nil {
-// 				log.Printf("Unable to query dims: %s", err)
-// 			} else {
-// 				// Encode dims as JSON
-// 				jsonDims := make(map[string]*json.RawMessage)
-// 				for name, dim := range dims {
-// 					encoded, err := json.Marshal(dim)
-// 					if err != nil {
-// 						log.Printf("Unable to json encode dim %s: %s", name, err)
-// 					} else {
-// 						raw := json.RawMessage(encoded)
-// 						jsonDims[name] = &raw
-// 					}
-// 				}
+			// Send buffered updates to client
+			oldUpdates.ForEach(func(item interface{}) {
+				update := item.(*streamingUpdate)
+				for _, client := range streamingClients {
+					client.updates <- update
+				}
+			})
+		case closedId := <-closedStreamingClient:
+			// Remove disconnected client from map
+			delete(streamingClients, closedId)
+		case <-time.After(waitTime):
+			// Query fallback and country dims
+			dims, err := QueryDims([]string{"fallback", "country"})
+			if err != nil {
+				log.Printf("Unable to query dims: %s", err)
+			} else {
+				// Encode dims as JSON
+				jsonDims := make(map[string]*json.RawMessage)
+				for name, dim := range dims {
+					encoded, err := json.Marshal(dim)
+					if err != nil {
+						log.Printf("Unable to json encode dim %s: %s", name, err)
+					} else {
+						raw := json.RawMessage(encoded)
+						jsonDims[name] = &raw
+					}
+				}
 
-// 				// Publish update to clients
-// 				update := &streamingUpdate{asOf: nextInterval, allDims: jsonDims}
-// 				for _, client := range streamingClients {
-// 					client.updates <- update
-// 				}
+				// Publish update to clients
+				update := &streamingUpdate{asOf: nextInterval, allDims: jsonDims}
+				for _, client := range streamingClients {
+					client.updates <- update
+				}
 
-// 				// Remember update
-// 				oldUpdates.Add(update)
-// 			}
-// 		}
-// 	}
-// }
+				// Remember update
+				oldUpdates.Add(update)
+			}
+		}
+	}
+}
 
-// // streamStats streams stats over a websocket
-// func streamStats(ws *websocket.Conn) {
-// 	dim, err := extractid(ws.Request())
-// 	if err != nil {
-// 		data, err := json.Marshal(&Response{Succeeded: false, Error: fmt.Sprintf("Unable to extract id from request: %s", err)})
-// 		if err == nil {
-// 			ws.Write(data)
-// 		}
-// 		return
-// 	}
+// streamStats streams stats over a websocket
+func streamStats(ws *websocket.Conn) {
+	dim, err := extractid(ws.Request())
+	if err != nil {
+		data, err := json.Marshal(&Response{Succeeded: false, Error: fmt.Sprintf("Unable to extract id from request: %s", err)})
+		if err == nil {
+			ws.Write(data)
+		}
+		return
+	}
 
-// 	dimNames := dimNamesFor(dim)
+	dimNames := dimNamesFor(dim)
 
-// 	client := &streamingClient{ws: ws, updates: make(chan *streamingUpdate, updatesCacheSize*2), id: make(chan int)}
+	client := &streamingClient{ws: ws, updates: make(chan *streamingUpdate, updatesCacheSize*2), id: make(chan int)}
 
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-// 	// Write updates to the client
-// 	go func() {
-// 		for {
-// 			// This gets data for all dims
-// 			update := <-client.updates
-// 			dims := update.allDims
-// 			if dimNames != nil {
-// 				// Grab only the dimensions that this client is interested in
-// 				dims = make(map[string]*json.RawMessage)
-// 				for _, name := range dimNames {
-// 					dims[name] = update.allDims[name]
-// 				}
-// 			}
-// 			resp := &StreamingQueryResponse{Response: Response{Succeeded: true}, AsOfSeconds: update.asOf.Unix(), Dims: dims}
-// 			encoded, err := json.Marshal(resp)
-// 			if err != nil {
-// 				log.Printf("Unable to marshal json: %s", err)
-// 			} else {
-// 				ws.Write(encoded)
-// 			}
-// 		}
-// 	}()
+	// Write updates to the client
+	go func() {
+		for {
+			// This gets data for all dims
+			update := <-client.updates
+			dims := update.allDims
+			if dimNames != nil {
+				// Grab only the dimensions that this client is interested in
+				dims = make(map[string]*json.RawMessage)
+				for _, name := range dimNames {
+					dims[name] = update.allDims[name]
+				}
+			}
+			resp := &StreamingQueryResponse{Response: Response{Succeeded: true}, AsOfSeconds: update.asOf.Unix(), Dims: dims}
+			encoded, err := json.Marshal(resp)
+			if err != nil {
+				log.Printf("Unable to marshal json: %s", err)
+			} else {
+				ws.Write(encoded)
+			}
+		}
+	}()
 
-// 	// Read from the client (we don't expect to get anything, but this allows us
-// 	// to check for closed connections)
-// 	go func() {
-// 		id := <-client.id
-// 		msg := make([]byte, 1)
-// 		for {
-// 			_, err := ws.Read(msg)
-// 			if err == io.EOF {
-// 				closedStreamingClient <- id
-// 				ws.Close()
-// 				wg.Done()
-// 				return
-// 			}
-// 		}
-// 	}()
+	// Read from the client (we don't expect to get anything, but this allows us
+	// to check for closed connections)
+	go func() {
+		id := <-client.id
+		msg := make([]byte, 1)
+		for {
+			_, err := ws.Read(msg)
+			if err == io.EOF {
+				closedStreamingClient <- id
+				ws.Close()
+				wg.Done()
+				return
+			}
+		}
+	}()
 
-// 	newStreamingClient <- client
-// 	wg.Wait()
-// }
+	newStreamingClient <- client
+	wg.Wait()
+}
