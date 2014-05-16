eec5f3e706a82a20b6173b49a433bf566b128691
Ox Cart <ox.to.a.cart@gmail.com>
2014-05-16 11:42:56 -0500

    #4 Not terminating just because I couldn't dial Redis

--- statshub/redis.go
+++ statshub/redis.go
@@ -16,8 +16,8 @@
 package statshub
 
 import (
+	"fmt"
 	"github.com/garyburd/redigo/redis"
-	"log"
 	"os"
 	"time"
 )
@@ -45,12 +45,13 @@ func init() {
 				redisConnectTimeout,
 				redisReadTimeout,
 				redisWriteTimeout)
+
 			if err != nil {
-				log.Fatalf("Unable to dial redis: %s", err)
+				return nil, fmt.Errorf("Unable to dial redis: %s", err)
 			}
 			if _, err := c.Do("AUTH", os.Getenv("REDIS_PASS")); err != nil {
 				c.Close()
-				log.Fatalf("Unable to authenticate to redis: %s", err)
+				return nil, fmt.Errorf("Unable to authenticate to redis: %s", err)
 			}
 			return c, err
 		},
