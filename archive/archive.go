bc6e6c285827453733cfdbbd594d680ea7b18647
Ox Cart <ox.to.a.cart@gmail.com>
2014-11-08 10:32:37 -0600

    Bumped natty stat intervals back to 1 hour

--- archive/archive.go
+++ archive/archive.go
@@ -41,9 +41,9 @@ func StartArchiving() {
 		archivePeriodically("country", 1*time.Hour)
 		//archivePeriodically("host", 1*time.Hour)
 		archivePeriodically("user", 24*time.Hour)
-		archivePeriodically("answerercountry", 10*time.Minute)
-		archivePeriodically("offereranswerercountries", 10*time.Minute)
-		archivePeriodically("operatingsystem", 10*time.Minute)
+		archivePeriodically("answerercountry", 1*time.Hour)
+		archivePeriodically("offereranswerercountries", 1*time.Hour)
+		archivePeriodically("operatingsystem", *time.Hour)
 	} else {
 		log.Printf("%s was not \"true\", not archiving to BigQuery", ARCHIVE_TO_BIGQUERY)
 	}
